d5c85d66dd7d25783446e30136c9c1c52f1d77e0
Nana <nana.d@hotmail.de>
2019-02-19 14:15:03 +0100

    fixed typo

--- Prime.java
+++ Prime.java
@@ -7,7 +7,7 @@ public class Prime {
 	//then, write another method findDoublette which takes an integer value as input and 
 	//is looking for the first prime-doublette it findes. A prime-doublette consists of
 	//two prime values with the difference of 2 (e.g. 3 and 5).
-	//The function should return the smaller one of the doulette. If there exists no
+	//The function should return the smaller one of the doublette. If there exists no
 	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
 	//the function should return 0.
 		
