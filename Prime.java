8f5034344fd46c03dc2e3ceab1d5e7d59954a26f
Nana <nana.d@hotmail.de>
2019-02-19 14:10:30 +0100

    enhanced prime exercise

--- Prime.java
+++ Prime.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 
 public class Prime {
-	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		
-		//todo: write a method which tells you if a given number is a prime or not
+	//todo: write a method isPrime which tells you if a given number is a prime or not
+	//then, write another method findDoublette which takes an integer value as input and 
+	//is looking for the first prime-doublette it findes. A prime-doublette consists of
+	//two prime values with the difference of 2 (e.g. 3 and 5).
+	//The function should return the smaller one of the doulette. If there exists no
+	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
+	//the function should return 0.
 		
-	}
 	
 }
