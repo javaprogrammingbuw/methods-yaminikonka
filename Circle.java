a38298d790c79a34ad3c2aaf16a04ce91c022807
Konka Yamini <yamini.konka@gmail.com>
2019-03-19 18:24:19 +0530

    today exercise, please check, if any improvements are needed , please let  me know--- thank you

--- Circle.java
+++ Circle.java
@@ -1,5 +1,33 @@
-
+// import Math.PI;
+import java.util.Scanner;
 public class Circle {
 	//todo: add some methods to compute a circle's diameter, circumference and area.
 	//print some outputs to confirm you methods are working right.
+	public static double diameter(double radius){
+		return 2*radius;
+	}
+	// public static double diameter(double x,double y){
+	// 	// the radius is calculating from the origin, i.e (0,0)
+	// 	return 2*Math.sqrt(x*x+y*y);
+	// }
+	public static double circumference(double dia){
+		return dia*Math.PI;
+	}
+	public static double area(double dia){
+		double r=dia/2.0;
+        return Math.PI*r*r;
+	}
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		
+
+		System.out.println("enter diameter of the circle");
+		double radius=scan.nextInt();
+		System.out.println("the diameter is: "+diameter(radius));
+		System.out.println("the circumference of the circle: "+circumference(diameter(radius)));
+		System.out.println("the area of the circle: "+area(diameter(radius)));
+
+		
+		
+	}
 }
--- Prime.java
+++ Prime.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 
 public class Prime {
-		
+
+
 	//todo: write a method isPrime which tells you if a given number is a prime or not
 	//then, write another method findDoublette which takes an integer value as input and 
 	//is looking for the first prime-doublette it findes. A prime-doublette consists of
@@ -10,6 +11,70 @@ public class Prime {
 	//The function should return the smaller one of the doublette. If there exists no
 	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
 	//the function should return 0.
+
+	public static boolean isPrime(int a){
+		int sum=0;
+		for(int i=1;i<a-1;i++){
+			if(a%i==0){
+				sum=sum+1;
+				if(sum>1){
+					return false;
+				}
+				// return true;
+			}
+				// return true;
+			
+		}
+		return true;
+	}
+	public static int findDoublette(int a){
+		int sum=0;
+		for(int j=a+1;j<Integer.MAX_VALUE;j++){
+			// for(int i=1;i<a-1;i++){
+			boolean b=isPrime(j);
+			     
+				if(b==true&&sum<1){
+                   boolean c=isPrime(j+2);
+                     if(c==true){
+                     	int x=j+2;
+                        System.out.println("the prime-doublette numbers are:"+j+" and "+x);
+                        sum=sum++;
+                        return j;
+                     }
+
+				}
+					
+				
+			// }
+		}
+		return 0;
+
+	}
+	// public static void smallerDoublette(int a){
+ //           int num=findDoublette(a);
+ //           if(num!=0){
+ //           	System.out.println("the smaller one is:"+num);
+ //           }else{
+ //           	System.out.println(num);
+ //           }
+	// }
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the number ");
+		int n=scan.nextInt();
+		boolean y=isPrime(n);
+		if(y==true){
+			System.out.println(n+" is prime number");
+		}else{
+			System.out.println(n+" is not a prime number");
+		}
+		// System.out.println("give a input for the finding the doublette");
+		// int s=scan.nextInt();
+		System.out.println("the smaller one is: "+findDoublette(n));
+		// smallerDoublette(s);
+
+	}
+
 		
 	
 }
